3f6d87483ab1cbd984467f694bc2dd1e158c0148
Christophe Benz <christophe.benz@gmail.com>
2015-10-02 17:55:44 +0200

    Give data as input

--- calculate_ars.py
+++ calculate_ars.py
@@ -1,12 +1,92 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 
 # import json
+import os
 
 import numpy as np
+import pandas as pd
 
 from openfisca_core import periods
 import openfisca_france
+
+
+script_dir = os.path.realpath(os.path.dirname(__file__))
+data_dir = os.path.realpath(os.path.join(script_dir, 'data'))
+
+
+def read_data(path):
+    # path_agen = os.path.join(path, 'agen_utile.csv')
+    # agen = pd.read_csv(path_agen, sep=';')
+
+    path_fil = os.path.join(path, 'fil_utile.csv')
+    fil = pd.read_csv(path_fil, sep=';')
+
+    ###
+    # données foyer familliales
+    # fam = fil['ARSVERS']
+    # NUMCOMDO ?
+    # fam2 = agen[['ARSVERS', 'MTARSVER']]
+
+    # données individuelles
+    # on part de la table par foyer
+
+    var_ages = ['DTNAIRES', 'DTNAICON']
+    var_age_enf = [u'ANNNEN1', u'ANNNEN2', u'ANNNEN3', u'ANNNEN4']
+    ind = fil[['PERSCOUV', 'SEXE'] + var_ages + var_age_enf]
+
+    nb_adulte = 1 + (ind.DTNAICON != 99)
+    nb_enf = (ind.PERSCOUV - nb_adulte)
+    nb_enf_autres = nb_enf - 4 * (ind.ANNNEN4.notnull())
+    nb_enf_autres[nb_enf_autres < 0] = 0
+
+    for var in var_age_enf:
+        ind[var].replace(0, np.nan, inplace=True)
+        ind[var] = 2013 - ind[var]
+
+    # sexe_conj = ~ind['SEXE']  # inutile en fait
+
+    for var in var_ages:
+        age = ind[var]
+        age[age == 99] = np.nan
+        age = 5 * (age - 2) + 20 + 2
+        age[age == 17] = 10
+        ind[var] = age
+
+    ind['idfam'] = ind.index
+
+    # quifam = 0
+    quifam0 = ind[['idfam', 'DTNAIRES']]
+    quifam0.columns = ['idfam', 'age']
+    quifam0['quifam'] = 0
+    # quifam = 0
+    quifam1 = ind.loc[ind['DTNAICON'].notnull(),
+                      ['idfam', 'DTNAICON']]
+    quifam1.columns = ['idfam', 'age']
+    quifam1['quifam'] = 1
+
+    list_enf = []
+    for num_enf in range(4):
+        var_enf_name = 'ANNNEN' + str(num_enf + 1)
+        var_enf = ind[var_enf_name]
+        enf = ind.loc[var_enf.notnull(), ['idfam', var_enf_name]]
+        enf.columns = ['idfam', 'age']
+        enf['quifam'] = 2 + num_enf + 1
+        list_enf += [enf]
+
+    count = 7
+    while sum(nb_enf_autres > 0) > 0:
+        cond = nb_enf_autres > 0
+        enf_autre = ind.loc[cond, ['idfam', 'ANNNEN4']]
+        enf_autre.columns = ['idfam', 'age']
+        enf_autre['age'] += count - 6
+        enf_autre['quifam'] = count
+        nb_enf_autres -= 1
+        count += 1
+        list_enf += [enf_autre]
+
+    individu = quifam0.append([quifam1] + list_enf)
+    return individu
 
 
 def main():
@@ -15,45 +95,23 @@ def main():
 
     scenario = tax_benefit_system.new_scenario()
     scenario.period = periods.period('2014')
-    # scenario.init_single_entity(
-    #     period = 2014,
-    #     parent1 = dict(
-    #         birth = '1970-01-01',
-    #         salaire_de_base = 1000,
-    #         ),
-    #     enfants = [
-    #         dict(
-    #             birth = '2000-02-01',
-    #             ),
-    #         dict(
-    #             birth = '2001-04-17',
-    #             ),
-    #         ],
-    #     )
+
+    individu = read_data(data_dir)
+
+    year_birth = (2014 - individu['age']).astype(int).astype(str)
+    individu['birth'] = year_birth + '-10-02'
+    individu['birth'] = pd.to_datetime(individu['birth'])
+    del individu['age']
 
     scenario.input_variables = {
-        variable_name: {periods.period('2014'): input_variable}
-        for variable_name, input_variable in {
-            'birth': np.array([np.datetime64('1970-01-01'), np.datetime64('2000-02-02'), np.datetime64('2001-02-02')]),
-            'idfam': np.array([0, 0, 0]),
-            'idfoy': np.array([0, 0, 0]),
-            'idmen': np.array([0, 0, 0]),
-            'quifam': np.array([0, 2, 3]),
-            'quifoy': np.array([0, 2, 3]),
-            'quimen': np.array([0, 2, 3]),
-            'salaire_de_base': np.array([1000, 0, 0]),
-            }.iteritems()
+        variable_name: {periods.period('2014'): serie.values}
+        for variable_name, serie in individu.iterkv()
         }
-    # scenario_json = scenario.to_json()
-    # simulation_json = {
-    #     'scenarios': [scenario_json],
-    #     'variables': ['ars'],
-    #     }
-    # print json.dumps(simulation_json, indent = 2)
 
     simulation = scenario.new_simulation()
     ars = simulation.calculate('ars')
     print ars
+    print len(ars)
 
 
 if __name__ == '__main__':
